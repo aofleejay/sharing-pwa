82c500ed62faa280e52a958fb409660277bb2604
kunapot <kunapot@thinknet.co.th>
2017-08-30 12:47:50 +0700

    remove react

--- build/sw.js
+++ build/sw.js
@@ -1,74 +1,42 @@
-const CSS_CACHE_KEY = 'index-css'
+const CSS_CACHE_KEY = 'cache-request-v5'
 const urlsToCache = [
-  'index.css',
-  'https://script.googleusercontent.com/macros/echo?user_content_key=0TTuuA87DEwQUExbmU3HKCbxTE-GjbbdU1BSmdbOUjqT69hp8wxEaDYO57fy-Hvb4Dtl9nxgYmJCSdssp3V41WYzDA5YEDHAm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJ9GRkcRevgjTvo8Dc32iw_BLJPcPfRdVKhJT5HNzQuXEeN3QFwl2n0M6ZmO-h7C6eIqWsDnSrEd&lib=MwxUjRcLr2qLlnVOLh12wSNkqcO1Ikdrk'
+  '/index.css',
 ]
 
-self.addEventListener('install', event => {
-  console.log('install')
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CSS_CACHE_KEY)
-      .then(cache => {
-        console.log('Adding Cache')
-        return cache.addAll(urlsToCache)
-      })
-  )
-})
-
-self.addEventListener('activate', event => {
-  console.log('activate')
-  event.waitUntil(
-    caches.keys().then(keys => {
-      keys.map(key => {
-        if(key !== CSS_CACHE_KEY) {
-          console.log(key)
-          return caches.delete(key)
-        }
-      })
+    caches.open(CSS_CACHE_KEY).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    }).then(() => {
+      return self.skipWaiting();      
     })
-  )
-  return self.clients.claim
-})
+  );
+});
 
-self.addEventListener('fetch', event => {
-  console.log('fecth')
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open(CSS_CACHE_KEY).then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
-        });
+        });  
       });
     })
   );
-  // console.log(event.request)
-  // event.respondWith(
-  //   caches.match(event.request)
-  //     .then(response => {
-  //       if (response) {
-  //         console.log('cache hit')          
-  //         return fetch(event.request)
-  //       } else {
-  //         console.log('cache missed')
-  //         const fetchRequest = event.request.clone()
-  //         return fetch(fetchRequest).then(response => {
-  //             if(!response || response.status !== 200 || response.type !== 'basic') {
-  //               return response
-  //             }
+});
 
-  //             const responseToCache = response.clone()
-  
-  //             caches.open(CSS_CACHE_KEY)
-  //               .then(cache => {
-  //                 cache.put(event.request, responseToCache)
-  //               })
-  
-  //             return response
-  //           }
-  //         )
-  //       }
-  //     }
-  //   )
-  // )
-})
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CSS_CACHE_KEY];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
