a1372594ab0586388b6f0c1b2d240c7e9e12e4ce
kunapot <kunapot@thinknet.co.th>
2017-09-14 23:18:19 +0700

    step 1 : add web app manifest

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,40 +1,6 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register("/service-worker.js").then(register => {
-      console.log('Register succeeded in scope : ', register.scope)
-    })
-  })
-} else {
-  console.log('Service worker not support.')
-}
-
-if (!('Notification' in window)) {
-  console.log("This browser does not support desktop notification")
-}
-
-else if (Notification.permission === 'granted') {
-  const notification = new Notification("Hello", {
-    body: 'This is push notification.',
-    icon: 'assets/images/logo.png',
-  })
-}
-
-else if (Notification.permission !== 'denied' || Notification.permission === 'default') {
-  Notification.requestPermission(permission => {
-    if (permission === 'granted') {
-      const notification = new Notification("Welcome", {
-        body: 'This is push notification.',
-        icon: 'assets/images/logo.png',
-      })
-    }
-  })
-}
-
 (function($){
   $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    $('.button-collapse').sideNav()
+    $('.parallax').parallax()
+  })
+})(jQuery)
