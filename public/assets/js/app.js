e93cde29d43dc1803b32517ed5fecc5484923c26
kunapot <kunapot@thinknet.co.th>
2017-10-05 14:29:58 +0700

    remove notification

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,28 +8,6 @@ if ('serviceWorker' in navigator) {
   console.log('Service worker not support.')
 }
 
-// if (!('Notification' in window)) {
-//   console.log("This browser does not support desktop notification")
-// }
-
-// else if (Notification.permission === 'granted') {
-//   const notification = new Notification("Hello", {
-//     body: 'This is push notification.',
-//     icon: 'assets/images/logo.png',
-//   })
-// }
-
-// else if (Notification.permission !== 'denied' || Notification.permission === 'default') {
-//   Notification.requestPermission(permission => {
-//     if (permission === 'granted') {
-//       const notification = new Notification("Welcome", {
-//         body: 'This is push notification.',
-//         icon: 'assets/images/logo.png',
-//       })
-//     }
-//   })
-// }
-
 (function($){
   $(function(){
     $('.button-collapse').sideNav()
